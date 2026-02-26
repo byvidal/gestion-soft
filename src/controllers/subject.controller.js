88aa409819927c6dbf65540966e890afe60140a8
byvidal <ibryanvidal@gmail.com>
2026-02-26 15:55:17 -0600

    tercer commit

--- src/controllers/subject.controller.js
+++ src/controllers/subject.controller.js
@@ -20,3 +20,23 @@ export const createSubject = async (req, res) => {
         res.status(500).json({ message: "Error al registrar la materia", error: error.message });
     }
 };
+
+export const getSubjects=async(req, res)=>{
+    const subject = await Subject.findById(req.params.id).populate('user');
+    if(!subject) return res.status(404).json({message:'Elemento no encontrado'})
+    res.json(subject)
+};
+
+export const deleteSubjects=async(req,res)=>{
+    const subject = await Subject.findByIdAndDelete(req.params.id)
+    if(!subject) return res.status(404).json({message:'Elemento no encontrado'})
+    return res.sendStatus(204);
+}
+
+export const updateSubjects=async(req,res)=>{
+    const subject = await Subject.findByIdAndUpdate(req.params.id,req.body,{
+        new:true
+    })
+    if(!subject) return res.status(404).json({message:'Elementos no encontrado'})
+    res.json(subject)
+}
--- src/routers/subject.routes.js
+++ src/routers/subject.routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { authRequired } from '../middlewares/validateToken.js';
 import { createSubject } from '../controllers/subject.controller.js';
+import { getSubjects, deleteSubjects, updateSubjects } from '../controllers/subject.controller.js';
 
 const router = Router();
 
-router.post('/subject', authRequired, createSubject);
+router.get('/subject:id', authRequired, getSubject);
+router.delete('/subject/:id', authRequired, deleteSubject);
+router.put('/subject/:id', authRequired, updateSubject);
 
 export default router;
