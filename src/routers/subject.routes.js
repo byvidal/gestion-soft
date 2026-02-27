1eb6a234385f7ea5981d9ad16448a882ac10e2e8
byvidal <ibryanvidal@gmail.com>
2026-02-27 14:40:27 -0600

    Tercer Commid

--- src/routers/subject.routes.js
+++ src/routers/subject.routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { authRequired } from '../middlewares/validateToken.js';
-import { createSubject } from '../controllers/subject.controller.js';
-import { getSubjects, deleteSubjects, updateSubjects } from '../controllers/subject.controller.js';
+import { createSubject, getSubjects, deleteSubjects, updateSubjects } from '../controllers/subject.controller.js'; 
 
 const router = Router();
 
-router.get('/subject:id', authRequired, getSubject);
-router.delete('/subject/:id', authRequired, deleteSubject);
-router.put('/subject/:id', authRequired, updateSubject);
+router.post('/subject', authRequired, createSubject); 
+router.get('/subject/:id', authRequired, getSubjects); 
+router.delete('/subject/:id', authRequired, deleteSubjects);
+router.put('/subject/:id', authRequired, updateSubjects);
 
 export default router;
